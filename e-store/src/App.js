d728d5abf3ed3d59a881d43b00f5c3b2d085d90d
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-02-11 20:34:08 -0500

    feat: add shop page with menu-items

--- e-store/src/App.js
+++ e-store/src/App.js
@@ -1,21 +1,15 @@
-import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = (props) => {
-  console.log(props);
-  return(
-    <div>
-      <h2> Hats </h2>
-    </div>
-  )};
+import './App.css';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/e-commerce/' component={HomePage} />
-        <Route path='/e-commerce/shop/hats/' component={HatsPage} />
+        <Route path='/e-commerce/shop' component={ShopPage} />
       </Switch>
     </div>
   );
