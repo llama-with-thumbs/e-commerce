25277af7362231a72da10123fe786d626bac6990
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-02-12 20:59:53 -0500

    feat: add header component

--- e-store/src/App.js
+++ e-store/src/App.js
@@ -1,14 +1,15 @@
 import HomePage from './pages/homepage/homepage.component';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/header.component';
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path='/e-commerce/' component={HomePage} />
+        <Route exact path='/e-commerce' component={HomePage} />
         <Route path='/e-commerce/shop' component={ShopPage} />
       </Switch>
     </div>
