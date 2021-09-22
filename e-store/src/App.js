4c335d727a3b9ff1867b3768e01cc8cf1db93896
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-09-22 19:40:57 -0400

    feat: add sign out button

--- e-store/src/App.js
+++ e-store/src/App.js
@@ -1,22 +1,46 @@
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
 import Header from './components/header/header.component';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/e-commerce' component={HomePage} />
-        <Route path='/e-commerce/shop' component={ShopPage} />
-        <Route path='/e-commerce/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/e-commerce" component={HomePage} />
+          <Route path="/e-commerce/shop" component={ShopPage} />
+          <Route path="/e-commerce/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- e-store/src/components/header/header.component.jsx
+++ e-store/src/components/header/header.component.jsx
@@ -1,27 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = () => (
-    <div className='header'>
-        <Link className='logo-container' to='/e-commerce/'>
-            <Logo className='logo' />
-        </Link>
-        <div className='options'>
-            <Link className='option' to='/e-commerce/shop'>
-                SHOP
-            </Link>
-            <Link className='option' to='/e-commerce/contact'>
-                CONTACT
-            </Link>
-            <Link className='option' to='/e-commerce/signin'>
-                SIGN IN
-            </Link>
+const Header = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/e-commerce/">
+      <Logo className="logo" />
+    </Link>
+    <div className="options">
+      <Link className="option" to="/e-commerce/shop">
+        SHOP
+      </Link>
+      <Link className="option" to="/e-commerce/contact">
+        CONTACT
+      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
         </div>
+      ) : (
+        <Link className="option" to="/e-commerce/signin">
+          SIGN IN
+        </Link>
+      )}
     </div>
-)
+  </div>
+);
 
 export default Header;
