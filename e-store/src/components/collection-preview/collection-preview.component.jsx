8ae2a04e5d6daef55c13bc142d05b57e3fa6321b
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-09-27 17:23:05 -0400

    feat: create cart-item component, add cart-item to cart-dropdown

--- e-store/src/components/collection-preview/collection-preview.component.jsx
+++ e-store/src/components/collection-preview/collection-preview.component.jsx
@@ -3,17 +3,17 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({ title, items}) => (
-    <div className='collection-preview'>
-        <h2 className='title'>{title.toUpperCase()}</h2>
-        <div className='preview'>
-            {items
-                .filter((item, i) => i < 4)
-                .map(({id, ...otherItemProps}) => (
-                <CollectionItem key={id} {...otherItemProps} />
-            ))}
-        </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h2 className="title">{title.toUpperCase()}</h2>
+    <div className="preview">
+      {items
+        .filter((item, i) => i < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
+  </div>
 );
 
 export default CollectionPreview;
