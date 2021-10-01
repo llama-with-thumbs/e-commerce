3643232fd5a6892691cd5256aefec0ad9cef0a63
llama-with-thumbs <vlad.filitovich@gmail.com>
2021-10-01 17:37:53 -0400

    feat: make cart items save in the lockal storage

--- e-store/src/redux/root-reducer.js
+++ e-store/src/redux/root-reducer.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import { persistReducer } from 'redux-persist';
+import directoryReducer from './directory/directory.reducer';
+import storage from 'redux-persist/lib/storage';
 
-export default combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   cart: cartReducer,
   user: userReducer,
+  directory: directoryReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
